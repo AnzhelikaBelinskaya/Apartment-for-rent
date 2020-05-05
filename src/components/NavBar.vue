aa7f0a37f6aed15f6ddaf20ace4427103e66efe1
Anzhelika Belinskaya <blindx@list.ru>
2020-05-06 00:20:43 +0300

    slider

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="nav-bar__container">
+        <NavButton title="Главная" url="https://www.yandex.ru" />
+        <NavButton title="Контакты" url="https://www.mail.ru" />
+    </div>
+</template>
+
+<script>
+import NavButton from './NavButton';
+export default {
+    components: {
+        NavButton
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.nav-bar {
+    &__container {
+        display: flex;
+    }
+}
+</style>
---
+++ src/components/NavButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <a class="nav-button__container" :href="url">
+        {{ title }}
+    </a>
+</template>
+
+<script>
+export default {
+    props: ['title', 'url']
+}
+</script>
+
+<style scoped lang="scss">
+.nav-button {
+    &__container {
+        padding: 4px 8px;
+        font-weight: bold;
+        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        color: #652387;
+        cursor: pointer;
+    }
+}
+</style>
